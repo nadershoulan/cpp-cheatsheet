eadfed1f3dae07c9c2b7aafffe3abffb476c15bf
nadershoulan <70449899+nadershoulan@users.noreply.github.com>
2020-10-04 11:12:33 +0300

    trail

--- cheatsheet-as-sourcefile.cpp
+++ cheatsheet-as-sourcefile.cpp
@@ -1,6 +1,2 @@
            + fib(i-2);
 };
-future<int> fut =         // result of async function
-        async(launch::async, fib, 4); // start async function in other thread
-// do some other work 
-cout << fut.get();        // get result of async function. Wait if needed.
